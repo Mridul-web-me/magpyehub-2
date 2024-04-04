51ba07cb6cc5245e70f43f4e4f8324a5082b2c6f
Mridul Islam <md.mridul.23@gmail.com>
2024-04-05 05:55:52 +0600

    spinner added

--- src/Component/Header/SearchBox/SearchResult/SearchResult.js
+++ src/Component/Header/SearchBox/SearchResult/SearchResult.js
@@ -7,48 +7,59 @@ import Newsletter from '../../../Newsletter/Newsletter';
 import AllProduct from '../../../Product/AllProduct/AllProduct';
 import PromoUnit from '../../../PromoUnit/PromoUnit';
 import Header from '../../Header';
+import { useForm } from 'react-hook-form';
 
 const SearchResult = () => {
-    // const { _id, title, img, category, price } = props.product;
-    const { searchText } = useParams()
-    const { handleAddToCart } = useProduct()
-    const [product, setProduct] = useState([]);
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        fetch(`https://magpyehub-server.onrender.com/products/search?search=${searchText}`)
-            .then(res => res.json())
-            .then(data => {
-                setProduct(data)
-                setLoading(false)
-            });
-        // console.log(product);
-    }, [searchText])
+  // const { _id, title, img, category, price } = props.product;
+  const { searchText } = useParams();
+  const { handleAddToCart } = useProduct();
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(`https://magpyehub-server.onrender.com/products/search?search=${searchText}`)
+      .then(res => res.json())
+      .then(data => {
+        setProduct(data);
+        setLoading(false);
+      });
+    console.log(product);
+  }, [searchText]);
 
-    return <div>
-        <Header></Header>
-        <PromoUnit></PromoUnit>
-        <Container>
-            {loading ? <div>
-                <Spinner animation="grow" variant="info" />
-                <Spinner animation="grow" variant="info" />
-                <Spinner animation="grow" variant="info" />
-            </div> : !product.length ? <div style={{ margin: '100px' }}> <h2 style={{ color: '#606060', fontWeight: '700' }}>No Product Found</h2> </div> : <Row xs={1} md={4} className="g-4">
-                {
-                    product.map(product =>
+  const {} = useForm < FieldValues > {};
 
-                        <AllProduct
-                            key={product._id}
-                            product={product}
-                            handleAddToCart={handleAddToCart}
-                        // pageCount={pageCount}
-                        ></AllProduct>
-                    )
-                }
-            </Row>}
-        </Container>
-        <Newsletter></Newsletter>
-        <Footer></Footer>
+  return (
+    <div>
+      <Header></Header>
+      <PromoUnit></PromoUnit>
+      <Container>
+        {loading ? (
+          <div>
+            <Spinner animation="grow" variant="info" />
+            <Spinner animation="grow" variant="info" />
+            <Spinner animation="grow" variant="info" />
+          </div>
+        ) : !product.length ? (
+          <div style={{ margin: '100px' }}>
+            {' '}
+            <h2 style={{ color: '#606060', fontWeight: '700' }}>No Product Found</h2>{' '}
+          </div>
+        ) : (
+          <Row xs={1} md={4} className="g-4">
+            {product.map(product => (
+              <AllProduct
+                key={product._id}
+                product={product}
+                handleAddToCart={handleAddToCart}
+                // pageCount={pageCount}
+              ></AllProduct>
+            ))}
+          </Row>
+        )}
+      </Container>
+      <Newsletter></Newsletter>
+      <Footer></Footer>
     </div>
+  );
 };
 
 export default SearchResult;
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,37 +1,58 @@
-import React from 'react'
-import About from '../About/About'
-import Footer from '../Footer/Footer'
-import Banner from '../Header/Banner/Banner'
-import Header from '../Header/Header'
-import Newsletter from '../Newsletter/Newsletter'
-import Brand from '../Product/Brand/Brand'
-import FavouriteProduct from '../Product/FavouriteProduct/FavouriteProduct'
-import Product from '../Product/Product'
-import './Home.css'
-import PromoUnit from '../PromoUnit/PromoUnit'
-import { Helmet } from 'react-helmet'
+import React, { useEffect, useState } from 'react';
+import About from '../About/About';
+import Footer from '../Footer/Footer';
+import Banner from '../Header/Banner/Banner';
+import Header from '../Header/Header';
+import Newsletter from '../Newsletter/Newsletter';
+import Brand from '../Product/Brand/Brand';
+import FavouriteProduct from '../Product/FavouriteProduct/FavouriteProduct';
+import Product from '../Product/Product';
+import './Home.css';
+import PromoUnit from '../PromoUnit/PromoUnit';
+import { Helmet } from 'react-helmet';
+import { Spinner } from 'react-bootstrap';
 
 const Home = () => {
-    return (
-        <div>
-            <Helmet>
-                <title>
-                    Magpyehub Online | Home
-                </title>
-                <meta name="description" content="This is Magpyehub Online Shop" />
-            </Helmet>
+  const [loading, setLoading] = useState(true);
+  const [products, setProducts] = useState([]);
 
-            <Header></Header>
-            <PromoUnit></PromoUnit>
-            <Banner></Banner>
-            <Product></Product>
-            <Brand></Brand>
-            <FavouriteProduct></FavouriteProduct>
-            <About></About>
-            <Newsletter></Newsletter>
-            <Footer></Footer>
+  useEffect(() => {
+    fetch(`https://magpyehub-server.onrender.com/products/`)
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data.products);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <Helmet>
+        <title>Magpyehub Online | Home</title>
+        <meta name="description" content="This is Magpyehub Online Shop" />
+      </Helmet>
+
+      {loading ? (
+        <div className="text-center" style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          {' '}
+          <Spinner animation="grow" variant="info" />
+          <Spinner animation="grow" variant="info" />
+          <Spinner animation="grow" variant="info" />
         </div>
-    )
-}
+      ) : (
+        <>
+          <Header></Header>
+          <PromoUnit></PromoUnit>
+          <Banner></Banner>
+          <Product></Product>
+          <Brand></Brand>
+          <FavouriteProduct></FavouriteProduct>
+          <About></About>
+          <Newsletter></Newsletter>
+          <Footer></Footer>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
